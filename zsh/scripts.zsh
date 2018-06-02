1fbd2d4b1795e27869acd29672e246fc3eed6e3b
ciaranRoche <ciaranroche@hotmail.co.uk>
2018-06-02 14:11:20 +0100

    cleaned up scripts and alias

---
+++ zsh/scripts.zsh
@@ -0,0 +1,27 @@
+add_remote () { repo="$(basename -s .git `git config --get remote.origin.url`)" ; git remote add $1 git@github.com:$1/$repo.git ; git fetch $1 }
+
+verify () { 
+    if [[ $# -eq 0 ]] ; then
+        echo 'usage: verify <branch name> <remote>'
+    else
+        if [ -z "$2" ]
+            then
+                add_remote 'upstream' ;
+        else
+            add_remote $2 ;
+        fi
+        git checkout $1 ;
+    fi
+}
+
+view_alias () {
+    cat /Users/ciaranroche/.dotfiles/zsh/alias.zsh
+}
+
+restart_zsh () {
+    source /Users/ciaranroche/.dotfiles/zsh/zshrc
+}
+
+mkcd () {
+    mkdir -p $1; cd $1;
+}
